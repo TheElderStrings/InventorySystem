89eefc70f908ebbe8efd296587df61f0e3dfb88b
Jesús <vixximpred@gmail.com>
2019-08-26 20:48:39 +0200

    Set up initial Loadout

---
+++ src/test/java/com/github/theelderstrings/model/loadout/LoadoutUnitTest.java
@@ -0,0 +1,10 @@
+package com.github.theelderstrings.model.loadout;
+
+import org.junit.Before;
+
+public class LoadoutUnitTest {
+  private Loadout loadout;
+
+  @Before
+  public void setUp() {}
+}
